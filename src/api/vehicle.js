f0ecc28f2bc7482bd8b944b6357aee25ee54c18a
12346 <1358517884@qq.com>
2023-06-08 11:33:48 +0800

    11

--- src/api/vehicle.js
+++ src/api/vehicle.js
@@ -1,4 +1,14 @@
 
-import request from '@/utils/request'
+import { createAPI } from '@/utils/request'
 
-export const getDepartment = () => request.get('/company/department')
+// 获取车辆类型
+export const getvehicles = () => createAPI('/truckType/simple', 'get')
+
+// 获取所有车辆列表
+export const getTruckTypeld = (obj) => createAPI('/truck/page', 'get', obj)
+
+// 获取已停用的车辆
+export const getUnusedTrucks = () => createAPI('/unWorkingTrucks', 'get')
+
+// 获取已启用的车辆
+export const getUsedTrucks = () => createAPI('/workingTrucks', 'get')
--- src/views/transit/vehicle.vue
+++ src/views/transit/vehicle.vue
@@ -1,6 +1,7 @@
 <!-- 车辆列表 -->
 <template>
   <div class="dashboard-container vehicle customer-list-box">
+    <!-- 头部 -->
     <div class="header">
 
       <div class="left">
@@ -12,9 +13,9 @@
         >
           <el-option
             v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
+            :key="item.id"
+            :label="item.name"
+            :value="item.name"
           >
           </el-option>
         </el-select>
@@ -22,6 +23,7 @@
 
       <div class="center">
         车牌号码：<input
+          v-model="License"
           type="text"
           placeholder="请输入车牌号码"
         />
@@ -29,49 +31,223 @@
 
       <div class="right">
         <el-button type="danger">搜索</el-button>
-        <el-button>重置</el-button>
+        <el-button @click="reset">重置</el-button>
       </div>
     </div>
+    <!-- 全部 -->
+    <div class="header-to">
+      <div><el-button @click="truck">全部 {{ total }}</el-button> </div>
+      <div><el-button @click="usable">可用 {{ usables }}</el-button> </div>
+      <div><el-button @click="outOfservice">停用 {{ service }}</el-button></div>
+    </div>
+    <!-- 表格 -->
+    <el-card class="box-card">
+      <div
+        slot="header"
+        class="clearfix"
+      >
+        <el-button
+          type="danger"
+          round
+          @click="add"
+        >新增车辆</el-button>
+      </div>
+      <template>
+        <el-table
+          ref="singleTable"
+          :data="tableData"
+          highlight-current-row
+          style="width: 100%"
+          @current-change="handleCurrentChange"
+        >
+          <el-table-column
+            label="序号"
+            type="index"
+            width="80"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="licensePlate"
+            label="车牌号码"
+            width="120"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="truckTypeName"
+            label="车辆类型"
+            width="120"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="driverNum"
+            label="司机数量"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="workStatus"
+            label="车辆状态"
+          >
+            <template v-slot="{ row }">
+              <span>{{ row.workStatus=== 1 ? "启用" : "停用" }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="deviceGpsId"
+            label="GPS设备ID"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="allowableLoad"
+            label="实载重量（T）"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="allowableVolume"
+            label="实载体积（方）"
+          >
+          </el-table-column>
+          <el-table-column
+            label="操作"
+            width="200"
+          >
+            <template>
+              <el-button
+                type="text"
+                size="small"
+              >查看详情</el-button>
+              <el-button
+                type="text"
+                size="small"
+              >启用</el-button>
+              <el-button
+                type="text"
+                size="small"
+              >配置司机</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+      <!-- 分页 -->
+      <div class="block">
+        <el-pagination
+          :current-page="truckForm.page"
+          :page-sizes="[5, 15, 20, 25]"
+          :page-size="truckForm.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        >
+        </el-pagination>
+      </div>
+    </el-card>
+    <!-- 新增车辆弹框 -->
+    <el-dialog
+      title="新增车辆"
+      :visible.sync="dialogVisible"
+      width="30%"
+    >
+      <el-form
+        :label-position="labelPosition"
+        label-width="80px"
+        :model="formLabelAlign"
+      >
+        <el-form-item label="名称">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-input v-model="formLabelAlign.region"></el-input>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+      </el-form>
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="dialogVisible = false"
+        >确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getDepartment } from '@/api/vehicle'
+import { getvehicles, getTruckTypeld, getUnusedTrucks, getUsedTrucks } from '@/api/vehicle'
 export default {
   name: 'Vehicle',
   data() {
     return {
-      options: [
-        {
-          value: '选项1',
-          label: '黄金糕'
-        },
-        {
-          value: '选项2',
-          label: '双皮奶'
-        },
-        {
-          value: '选项3',
-          label: '蚵仔煎'
-        },
-        {
-          value: '选项4',
-          label: '龙须面'
-        },
-        {
-          value: '选项5',
-          label: '北京烤鸭'
-        }
-      ],
-      value: ''
+      options: [], // 车辆类型数组
+      value: '', // 车辆类型
+      License: '', // 车辆号码
+      tableData: [], // 表格数组
+      usables: 0, // 可用车辆数量
+      service: 0, // 停用车辆数量
+      total: 0, // 总数量
+      dialogVisible: false, // 新增车辆弹框
+      truckForm: {
+        page: 1,
+        pageSize: 5
+      }
     }
   },
   created() {
-    this.vehicle()
+    this.vehicle() // 车辆类型
+    // this.usable()// 可用车辆
+    // this.outOfservice()// 停用车辆
+    this.truck()// 全部
   },
   methods: {
+    // 获取页面车辆类型数据
     async vehicle() {
-      const res = await getDepartment()
-      console.log(res)
+      const res = await getvehicles()
+      this.options = res.data
+    },
+    // 获取车辆类型列表  全部
+    async truck() {
+      const res = await getTruckTypeld(this.truckForm)
+      this.total = res.data.counts // 获取总数
+      this.tableData = res.data.items// 获取数组
+    },
+    // 重置车辆选择
+    reset() {
+      this.value = ''
+      this.License = ''
+    },
+    // 可用的车辆
+    async usable() {
+      const res = await getUsedTrucks()
+      this.usables = res.data.length
+      this.tableData = res.data// 获取数组
+    },
+    // 停用的车辆
+    async outOfservice() {
+      const res = await getUnusedTrucks()
+      this.service = res.data.length
+      this.tableData = res.data// 获取数组
+    },
+    // 添加车辆
+    add() {
+      this.dialogVisible = true
+    },
+    setCurrent(row) {
+      this.$refs.singleTable.setCurrentRow(row)
+    },
+    // 每页多少条
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+      this.truckForm.pageSize = val
+      this.truck()
+    },
+    // 当前多少页
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+      this.truckForm.page = val
+      this.truck()
     }
   }
 }
@@ -159,11 +335,51 @@ export default {
     height: 60px;
   }
 }
+.header-to {
+  margin-top: 20px;
+  height: 50px;
+  background-color: #fff;
+  display: flex;
+  div {
+    width: 120px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+  }
+}
+.block{
+  text-align: center;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 5px;
+}
 
 // 下拉框
 /deep/ .el-select {
   width: 100%;
 }
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    margin-top: 20px;
+  }
 </style>
 
 <style>
