57db9c56e1ad3475fc8efb727e31064ac2556e5d
wang <2772818698@qq.com>
2023-06-09 20:33:43 +0800

    已上传

--- src/views/transit/freight-manage.vue
+++ src/views/transit/freight-manage.vue
@@ -25,10 +25,24 @@
             prop="transportType"
             label="运送类型"
           />
-          <el-table-column
+          <!-- <el-table-column
             prop="associatedCityList"
             label="关联城市"
           />
+          <template #default="{row}">
+            {{ row }}
+          </template> -->
+          <el-table-column
+            prop="associatedCityList"
+            label="关联城市"
+          >
+            <template v-slot="{ row }">
+              <span>
+                {{ row.associatedCityList | formateCity }}
+              </span>
+            </template>
+          </el-table-column>
+
           <el-table-column
             prop="firstWeight"
             label="首重"
@@ -45,18 +59,18 @@
             label="操作"
             align="center"
           >
-            <template v-slot="{row}">
+            <template v-slot="{ row }">
               <el-button
                 size="mini"
                 type="text"
                 @click="posttemplate(row.id)"
               >编辑
               </el-button>
-              <span style="color:#ccc">|</span>
+              <span style="color: #ccc">|</span>
               <el-button
                 size="mini"
                 type="text"
-                style="color: red;"
+                style="color: red"
                 @click="delfreights(row.id)"
               >删除
               </el-button>
@@ -130,11 +144,22 @@
         </el-form-item>
 
         <el-form-item
-          v-model="form.associatedCityList"
+          v-model="form.associatedCityList[1]"
           label="关联城市"
           prop="associatedCityList"
         >
-          全国
+          <span v-if="form.templateType === '3'">
+            <el-checkbox-group v-model="form.associatedCityList">
+              <el-checkbox label="2">京津冀</el-checkbox>
+              <el-checkbox label="3">江浙沪</el-checkbox>
+              <el-checkbox label="4">川渝</el-checkbox>
+              <el-checkbox label="5">黑吉辽</el-checkbox>
+            </el-checkbox-group>
+          </span>
+          <span
+            v-else
+            label="全国"
+          >全国</span>
         </el-form-item>
 
         <el-form-item
@@ -144,9 +169,9 @@
           <el-input
             v-model.number="form.firstWeight"
             placeholder="请输入首重价格"
-            style="position:absolute;"
+            style="position: absolute"
           ></el-input>
-          <span style="position:relative;float: right;left:-20px">元</span>
+          <span style="position: relative; float: right; left: -20px">元</span>
         </el-form-item>
 
         <el-form-item
@@ -156,10 +181,9 @@
           <el-input
             v-model.number="form.continuousWeight"
             placeholder="请输入续重价格"
-            style="position:absolute;"
+            style="position: absolute"
           ></el-input>
-          <span style="position:relative;float: right;left:-20px">元</span>
-
+          <span style="position: relative; float: right; left: -20px">元</span>
         </el-form-item>
 
         <el-form-item
@@ -172,27 +196,65 @@
           ></el-input>
         </el-form-item>
 
-        <span
-          style="display: flex; justify-content: center;"
-        >
+        <span style="display: flex; justify-content: center">
           <el-button
             type="primary"
-            style=" background-color:#e15536"
+            style="background-color: #e15536"
             @click="queding"
           >确 定</el-button>
           <el-button @click="btnCancel">取 消</el-button>
         </span>
       </el-form>
     </el-dialog>
-
   </div>
 </template>
 
 <script>
 import { getFreightList, delfreight, addfreight } from '@/api/freightManage'
 export default {
+  filters: {
+    formateCity(ids) {
+      for (const key in ids) {
+        if (ids[key] === '1') {
+          ids[key] = '全国'
+        } else if (ids[key] === '2') {
+          ids[key] = '京津冀'
+        } else if (ids[key] === '3') {
+          ids[key] = '江浙沪'
+        } else if (ids[key] === '4') {
+          ids[key] = '川渝'
+        } else if (ids[key] === '5') {
+          ids[key] = '黑吉辽'
+        }
+      }
+      return ids.join(',')
+      // const newArr = ids.map(item => {
+      //   if (item === '1') {
+      //     item = '全国'qq
+      //   } else if (item === '2') {
+      //     item = '京津冀'
+      //   } else if (item === '3') {
+      //     item = '江浙沪'
+      //   } else if (item === '4') {
+      //     item = '川渝'
+      //   } else if (item === '5') {
+      //     item = '黑吉辽'
+      //   }
+      // })
+      // console.log(newArr)
+      // return newArr.join(', ')
+      // console.log(ids['0'])
+    }
+  },
   data() {
     return {
+      city: {
+        1: '全国',
+        2: '京津冀',
+        3: '江浙沪',
+        4: '川渝',
+        5: '黑吉辽'
+      },
       list: [], // 获取数据
       dialogVisible: false, // 控制提示框的开关
       form: {
@@ -209,17 +271,30 @@ export default {
       },
       // 输入框校验
       rules: {
-        templateType: [{ required: true, message: '请选择模板类型', trigger: 'blur' }],
-        transportType: [{ required: true, message: '清选择运送类型', trigger: 'blur' }],
+        templateType: [
+          { required: true, message: '请选择模板类型', trigger: 'blur' }
+        ],
+        transportType: [
+          { required: true, message: '清选择运送类型', trigger: 'blur' }
+        ],
         // associatedCityList: [{ required: true, message: '全国', trigger: 'blur' }],
-        firstWeight: [{ required: true, message: '首重价格不能为空', trigger: 'blur' }, { type: 'number', message: '首重必须为数字值' }],
-        continuousWeight: [{ required: true, message: '续重价格不能为空', trigger: 'blur' }, { type: 'number', message: '续重必须为数字值' }],
-        lightThrowingCoefficient: [{ required: true, message: '轻抛系数不能为空', trigger: 'blur' }, { type: 'number', message: '轻抛系数必须为数字值' }]
+        firstWeight: [
+          { required: true, message: '首重价格不能为空', trigger: 'blur' },
+          { type: 'number', message: '首重必须为数字值' }
+        ],
+        continuousWeight: [
+          { required: true, message: '续重价格不能为空', trigger: 'blur' },
+          { type: 'number', message: '续重必须为数字值' }
+        ],
+        lightThrowingCoefficient: [
+          { required: true, message: '轻抛系数不能为空', trigger: 'blur' },
+          { type: 'number', message: '轻抛系数必须为数字值' }
+        ]
       }
     }
   },
   created() {
-    this.getList()// 获取运费列表
+    this.getList() // 获取运费列表
   },
   methods: {
     // 对话框
@@ -231,7 +306,7 @@ export default {
     async getList() {
       const { data } = await getFreightList()
       this.list = data
-      this.list.forEach(e => {
+      this.list.forEach((e) => {
         // 模板 数字判断字符串
         if (e.templateType === 1) {
           e.templateType = '同城寄'
@@ -243,9 +318,51 @@ export default {
           e.templateType = '跨省'
         }
         // 运送 数字判断字符串
-        e.transportType === 1 ? (e.transportType = '普快') : (e.transportType = '特快')
+        e.transportType === 1
+          ? (e.transportType = '普快')
+          : (e.transportType = '特快')
         // 关联 数字判断字符串
-        e.associatedCityList === 1 ? (e.associatedCityList = '全国') : (e.associatedCityList = '京津冀')
+        // e.associatedCityList === 1 ? (e.associatedCityList = '全国') : (e.associatedCityList = '京津冀')
+        // if (e.associatedCityList.includes('1')) {
+        //   e.associatedCityList = '全国'
+        // } else if (e.associatedCityList.includes('2')) {
+        //   e.associatedCityList = '京津冀'
+        // } else if (e.associatedCityList.includes('3')) {
+        //   e.associatedCityList = '江浙沪'
+        // } else if (e.associatedCityList.includes('4')) {
+        //   e.associatedCityList = '川渝'
+        // } else if (e.associatedCityList.includes('5')) {
+        //   e.associatedCityList = '黑辽宁'
+        // } else if (e.associatedCityList.includes('2', '3')) {
+        //   e.associatedCityList = '京津冀,江浙沪'
+        // } else if (e.associatedCityList.includes('2', '4')) {
+        //   e.associatedCityList = '京津冀,川渝'
+        // }
+
+        // e.associatedCityList = e.associatedCityList.map(city => {
+        //   if (city === 1) {
+        //     return '全国'
+        //   }
+        //   if (city.includes('2')) {
+        //     return '京津冀'
+        //   }
+        //   if (city.includes('3')) {
+        //     return '江浙沪'
+        //   }
+        //   if (city.includes('4')) {
+        //     return '川渝'
+        //   }
+        //   if (city.includes('5')) {
+        //     return '黑辽宁'
+        //   }
+        //   return city
+        // })
+
+        // if (this.list.associatedCityList.includes('1')) {
+        //   return '全国'
+        // } else {
+        //   return this.list
+        // }
       })
     },
     // 添加
@@ -275,12 +392,16 @@ export default {
         this.form.templateType = '跨省'
       }
       // 运送 数字判断字符串
-      this.form.transportType === 1 ? (this.form.transportType = '普快') : (this.form.transportType = '特快')
+      this.form.transportType === 1
+        ? (this.form.transportType = '普快')
+        : (this.form.transportType = '特快')
       // 关联 数字判断字符串
-      this.form.associatedCityList === 1 ? (this.form.associatedCityList = '全国') : (this.form.associatedCityList = '京津冀')
+      this.form.associatedCityList === 1
+        ? (this.form.associatedCityList = '全国')
+        : (this.form.associatedCityList = '京津冀')
     },
     // 删除
-    async  delfreights(id) {
+    async delfreights(id) {
       // 删除请求
       await delfreight(id)
       // 提示信息
@@ -291,7 +412,7 @@ export default {
     // 确认的时候
     queding() {
       // 确定的时候 校验表单
-      this.$refs.form.validate(async(isok) => {
+      this.$refs.form.validate(async (isok) => {
         if (isok) {
           if (this.form.id) {
             // 发编辑请求的时候需要把字符串重新转回数字 ，不然请求参数错误500
@@ -305,9 +426,13 @@ export default {
               this.form.templateType = 4
             }
             // 运送
-            this.form.transportType === '普快' ? (this.form.transportType = 1) : (this.form.transportType = 2)
+            this.form.transportType === '普快'
+              ? (this.form.transportType = 1)
+              : (this.form.transportType = 2)
             // 关联
-            this.form.associatedCityList === '全国' ? (this.form.associatedCityList = 1) : (this.form.associatedCityList = [2])
+            this.form.associatedCityList === '全国'
+              ? (this.form.associatedCityList = 1)
+              : (this.form.associatedCityList = [2])
 
             const res = await addfreight(this.form)
             if (res.msg === 'ok') {
@@ -355,7 +480,7 @@ export default {
     background-color: #fff;
     margin: 20px;
     // border-radius: 10px;
-    .tables{
+    .tables {
       width: 96%;
     }
     .btn-add {
@@ -367,13 +492,12 @@ export default {
       margin: 25px;
       font-size: 14px;
     }
-    .el-tables{
+    .el-tables {
       margin-left: 25px;
     }
-.ipt{
-    position: absolute;
-
-}
+    .ipt {
+      position: absolute;
+    }
   }
 }
 </style>
